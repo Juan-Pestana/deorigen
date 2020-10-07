bfafd4d0dd4742fee1b683329f3042761ff2cd34
Juan Pestana <juan.pestana82@gmail.com>
2020-10-07 15:47:51 +0200

    preparando pagina de perfil

--- client/src/components/pages/account/Account.js
+++ client/src/components/pages/account/Account.js
@@ -1,18 +1,43 @@
 import React, {Component} from 'react'
 
+import storeService from './../../../services/store.services'
+
 class Account extends Component {
     constructor(){
         super()
         this.state ={
+            user : {},
+            store: {}
 
         }
+        this.storeService = new storeService()
     }
+
+    // componentDidMount = () => this.setUser()
+
+    // setUser =() => {
+
+    //     this.props.loggedInUser.store ? 
+    //     this.storeService
+    //         .getOneStore(this.props.loggedInUser.store)
+    //         .then(this.state.store = response.data)
+
+  
+        // this.userService
+        //     .getOneUser(this.props.loggedInUser._id)
+        //     .then(response => this.setState({user: response.data}))
+        //     .catch(err => console.log('Error:', err))
+
+    // }
+
+
 
 
     render(){
+         console.log(this.state.user)
         return(
         <>
-            <h1>Hola {this.props.loggedInUser.firstName}</h1>
+            <h1>Hola {this.props.loggedInUser.store}</h1>
         </>
         )
     }
