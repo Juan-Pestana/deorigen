4b40f1db72702eb033c7e403f5f176d7bfb4bdfa
Javier de Salas <chinostrike@gmail.com>
2020-10-10 10:39:35 +0200

    accessing Bootstrap Sass

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './override.css'
 import App from './components/App';
 import { BrowserRouter as Router } from 'react-router-dom'
 
