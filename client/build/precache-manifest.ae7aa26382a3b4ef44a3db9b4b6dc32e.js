05774b2769d09d1fd1a775e842267cef198b41b9
Juan Pestana <juan.pestana82@gmail.com>
2020-10-15 20:02:32 +0200

    añadiendo build

---
+++ client/build/precache-manifest.ae7aa26382a3b4ef44a3db9b4b6dc32e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f521af4b20421eae89231558d0ce5d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4971cf51da99729c9962",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "47ff0234f4a34835243d",
+    "url": "/static/css/main.63880d05.chunk.css"
+  },
+  {
+    "revision": "4971cf51da99729c9962",
+    "url": "/static/js/2.f1b8513f.chunk.js"
+  },
+  {
+    "revision": "d3eab2c800484648f446253871549a7a",
+    "url": "/static/js/2.f1b8513f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47ff0234f4a34835243d",
+    "url": "/static/js/main.279b81c2.chunk.js"
+  },
+  {
+    "revision": "194cb280b99f00116224",
+    "url": "/static/js/runtime-main.bf49f6c1.js"
+  },
+  {
+    "revision": "e5366734cde41babfe7b5a6115d90318",
+    "url": "/static/media/LogoHome.e5366734.svg"
+  }
+]);
